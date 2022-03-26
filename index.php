95cb6fa41b74a5c856c013420c6898efb148bd57
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-03-26 00:44:15 +0000

    Added introductory text in php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Metadata -->
+    <meta charset="utf-8">
+    <meta name="description" content="My first webpage with user input">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="MariaG">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Code for the favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="manifest" href="./favicon/site.webmanifest">
+    <!-- Link to CSS file -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- Title -->
+    <title>Area and Perimeter of a Triangle with User Input in PHP- Maria Goemans</title>
+  </head>
+  <body>
+    <?php
+      echo "<h1>Calculating the Area and Perimeter of a Triangle in PHP with User Input</h1>";
+      echo "<h3>Explanation:</h3>";
+      echo "<p>Welcome to my webpage! Input the side lengths and height of a triangle below, and the program I've written will calculate the triangle's area and perimeter for you!</p>";
+    ?>
+  </body>
+</html>
